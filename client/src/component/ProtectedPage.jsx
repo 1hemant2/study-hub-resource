9fc96493eb7d0a32cb220f67e441b373f1cbc250
hemant <hk443957@gmail.com>
2024-01-16 01:24:07 +0530

    3. i have to first make the navbar responsive

--- client/src/component/ProtectedPage.jsx
+++ client/src/component/ProtectedPage.jsx
@@ -1,58 +1,70 @@
 import { Footer, Header } from 'antd/es/layout/layout'
-import React from 'react';
+import React, { useState } from 'react';
 import Searchs from '../Pages/Searchs/Searchs';
 import { useNavigate } from 'react-router-dom';
 
 const ProtectedPage = ({ children }) => {
     const navigate = useNavigate();
+    const [mobileMenu, setmobileMenu] = useState(false);
     return (
         <div>
-            <Header className='bg-gray-50 flex flex-row w-full '>
-                {/* subjects */}
+            <header className='bg-gray-50 w-full '>
+                <div className='flex flex-row'>
 
-                <div className='flex flex-row items-center w-6/12'>
-                    <div className='m-2 text-lg cursor-pointer text-blue-700'
-                        onClick={() => {
-                            navigate('/')
-                        }}
-                    >Home</div>
-                    <div className='m-2 text-lg cursor-pointer text-blue-700'
-                        onClick={() => {
-                            navigate('/htmls')
-                        }}
-                    >HTML</div>
-                    <div className='m-2 text-lg cursor-pointer text-blue-700'
-                        onClick={() => {
-                            navigate('/csss')
-                        }}
-                    >CSS</div>
-                    <div className='m-2 text-lg cursor-pointer text-blue-700 '
-                        onClick={() => {
-                            navigate('/javascripts')
-                        }}
-                    >JavaScript</div>
-                    <div className='m-2 text-lg cursor-pointer text-blue-700'
-                        onClick={() => {
-                            navigate('/reacts')
-                        }}
-                    >React</div>
-                    <div className='m-2 text-lg cursor-pointer text-blue-700'
-                        onClick={() => {
-                            navigate('/mysql')
-                        }}
-                    >MySql</div>
-                </div>
-                <div className='flex items-center  ml-10 text-red-500 w-3/12 text-3xl '>
-                    Study Hub
-                </div>
-                {/* search */}
-                <div className='flex justify-end items-center w-3/12 mt-5 sm:w-1/2'>
-                    <div >
+                    <div className='flex items-center justify-start  ml-10 text-red-500 w-1/5 text-3xl '>
+                        Study Hub
+                    </div>
+                    {/* search */}
+
+                    <div className='w-3/5 flex justify-center items-center'>
                         <Searchs></Searchs>
                     </div>
-
                 </div>
-            </Header>
+                <div className='text-4xl w-1/5 flex justify-end items-center' onClick={
+                    () => {
+                        setmobileMenu(!mobileMenu);
+                    }
+                }>
+                    <i className="ri-menu-line"></i>
+                </div>
+                <div >
+                    {
+                        mobileMenu ? (<div className='flex flex-col items-center '>
+                            <div className='m-2 text-lg cursor-pointer text-blue-700 '
+                                onClick={() => {
+                                    navigate('/')
+                                }}
+                            >Home</div>
+                            <div className='m-2 text-lg cursor-pointer text-blue-700'
+                                onClick={() => {
+                                    navigate('/htmls')
+                                }}
+                            >HTML</div>
+                            <div className='m-2 text-lg cursor-pointer text-blue-700'
+                                onClick={() => {
+                                    navigate('/csss')
+                                }}
+                            >CSS</div>
+                            <div className='m-2 text-lg cursor-pointer text-blue-700 '
+                                onClick={() => {
+                                    navigate('/javascripts')
+                                }}
+                            >JavaScript</div>
+                            <div className='m-2 text-lg cursor-pointer text-blue-700'
+                                onClick={() => {
+                                    navigate('/reacts')
+                                }}
+                            >React</div>
+                            <div className='m-2 text-lg cursor-pointer text-blue-700'
+                                onClick={() => {
+                                    navigate('/mysql')
+                                }}
+                            >MySql</div>
+                        </div>) : null
+                    }
+                </div>
+
+            </header>
             <main>
                 <div className='min-h-screen'>{children}</div>
             </main>
